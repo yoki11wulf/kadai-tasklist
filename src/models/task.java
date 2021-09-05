25a5a026119ffcfdc173c9457f3dd0e3a0a26434
yuuto.ooki <earth.423.eco@gmail.com>
2021-09-05 18:57:42 +0900

    getAllTaskesを追加

--- src/models/task.java
+++ src/models/task.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tasklist")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTaskes",
+            query = "SELECT m FROM task AS m ORDER BY m.id DESC"
+            )
+})
+@Table(name = "tasks")
 public class task {
     @Id
     @Column(name = "id")
