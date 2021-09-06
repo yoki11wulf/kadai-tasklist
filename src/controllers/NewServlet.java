37cb08ea7c6f3fce3dcbbea029192bca32b9f8b5
yuuto.ooki <earth.423.eco@gmail.com>
2021-09-06 23:35:27 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,25 +29,12 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
-	    em.getTransaction().begin();
+	    request.setAttribute("_token", request.getSession().getId());
 	    
-	    task t = new task();
+	    request.setAttribute("task", new task());
 	    
-	    String title = "study";
-	    t.setTitle(title);
-	    
-	    String content = "programming-java-study";
-	    t.setContent(content);
-	    
-	    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-	    t.setCreated_at(currentTime);
-	    t.setUpdated_at(currentTime);
-	    
-	    em.persist(t);
-	    em.getTransaction().commit();
-	    
-	    response.getWriter().append(Integer.valueOf(t.getId()).toString());
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+	    rd.forward(request, response);
 	}
 
 }
